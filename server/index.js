949ff025032f84488d7cdbc91258e49d73293f72
Anirban-02 <99980141+Anirban-02@users.noreply.github.com>
2023-12-06 02:50:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const app=express();
 const port=process.env.PORT||5000;
 const {orderConfirmMail}=require('./appController.js');
 app.use(express.json());
-mongoose.connect("mongodb://127.0.0.1:27017/ArtPage").then(()=>console.log("Connected to Database"))
+app.use(cors(
+    {
+        origin:[],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
+mongoose.connect("mongodb+srv://tataighosh104:Anipubg07@cluster0.ysntczx.mongodb.net/Artpage?retryWrites=true&w=majority").then(()=>console.log("Connected to Database"))
 .catch((err)=>console.log(err));
 
 const storage=multer.diskStorage({
